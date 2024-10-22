03112cb2fb93b26cec2db610973602302975e477
LamourMarine <marine.lamour@gmx.fr>
2024-10-22 16:22:25 +0200

    finSansBigBonus

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const Btn = document.querySelectorAll("input[type=button]");
-const imageElement = document.querySelector("image");
-const textContentElement = document.getElementById("textContent");
 
-let images = ["tour_eiffel.webp", "notre_dame.webp", "arc_de_triomphe.webp", "mont_saint_michel.webp", "versailles.webp"];
+function afficherTexte(titre, contenu) {
+    // Met à jour le titre de la section
+    document.getElementById('title').innerText = titre;
+    
+    // Met à jour le texte de la section
+    document.getElementById('text-content').innerText = contenu;
+}
 
 
-Btn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        let image = textContentElement
+
+const images = document.querySelectorAll('.image-row img');
+console.log(images);
+
+        images.forEach(image => {
+            image.addEventListener('mouseover', () => {
+                images.forEach(img => {
+                    if (img !== image) {
+                        img.style.filter = 'grayscale(100%)' ;
+                    }
+                    });
+
+                    afficherTexte(image.getAttribute('titre'), image.getAttribute('contenu'));
+                });
+                image.addEventListener('mouseout',() => {
+                    images.forEach(img => {
+                        img.style.filter = 'none';
+                    });
+
+                    });
+            });
+
         
